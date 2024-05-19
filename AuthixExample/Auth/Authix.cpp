eca3a6ea6ea74bdabf0de03798be1403c19ef5bf
Smartboy#2022 <83659036+Smartboy1337@users.noreply.github.com>
2024-05-19 22:42:20 +0200

    Update Authix.cpp

--- AuthixExample/Auth/Authix.cpp
+++ AuthixExample/Auth/Authix.cpp
@@ -1,9 +1,9 @@
 #include "Authix.hpp"
 #include "Decryption.hpp"
 
-std::string OwnerUUID = xorstr_("9be44ee9-d7b2-4703-bc7e-cd6e7332e19b");
-std::string AppName = xorstr_("BwPaidTemp");
-std::string AppSecretKey = xorstr_("092add00674a85f9231ee63f3d732e26445bb9a6bace6c35475769956f0e1fd4");
+std::string OwnerUUID = xorstr_("00000000-0000-0000-0000-00000000000");
+std::string AppName = xorstr_("Example");
+std::string AppSecretKey = xorstr_("000000000000000000000000000000000000000000000");
 
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output)
 {
@@ -90,6 +90,30 @@ namespace Authix
 		curl_easy_setopt(hnd, CURLOPT_WRITEDATA, &response_string);
 
 		auto command = std::format(("{{\"var_name\":\"{}\"}}"), VariableName);
+		curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, command.c_str());
+		curl_easy_perform(hnd);
+
+		return response_string;
+	}
+	std::string LogMessage(std::string Message, std::string SessionID)
+	{
+		CURL* hnd = curl_easy_init();
+		curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, xorstr_("POST"));
+		curl_easy_setopt(hnd, CURLOPT_WRITEDATA, stdout);
+		curl_easy_setopt(hnd, CURLOPT_URL, xorstr_("https://api.authix.cc/log"));
+
+		struct curl_slist* headers = NULL;
+		headers = curl_slist_append(headers, xorstr_("accept: application/json"));
+		headers = curl_slist_append(headers, (xorstr_("x-session-id: ") + SessionID).c_str());
+		headers = curl_slist_append(headers, xorstr_("content-type: application/json"));
+		headers = curl_slist_append(headers, xorstr_("User-Agent: AuthixExample"));
+		curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+		std::string response_string;
+		curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(hnd, CURLOPT_WRITEDATA, &response_string);
+
+		auto command = std::format(("{{\"message\":\"{}\"}}"), Message);
 		curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, command.c_str());
 		curl_easy_perform(hnd);
 
