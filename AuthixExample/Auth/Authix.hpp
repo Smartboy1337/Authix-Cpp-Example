d2104eadf30c2fb127d7aed0c93dbf5d5887131c
Smartboy#2022 <83659036+Smartboy1337@users.noreply.github.com>
2024-05-19 22:42:34 +0200

    Update Authix.hpp

--- AuthixExample/Auth/Authix.hpp
+++ AuthixExample/Auth/Authix.hpp
@@ -14,6 +14,7 @@ namespace Authix
 
 	std::string Login(std::string License, std::string Hwid, std::string SessionID);
 	std::string GetVariable(std::string VariableName, std::string SessionID);
+	std::string LogMessage(std::string Message, std::string SessionID);
 
 	std::string DecryptNextData(std::string Data, std::string NewIV);
 }
